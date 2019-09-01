30a17779986d99909ed95baec893ded16875f8a0
hehehahaha <gejiexian@126.com>
2019-09-01 11:50:53 +0800

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -14,3 +14,17 @@ class Solution:
             for i in xrange(2,n):
                 s.append(s[i-1]+s[i-2])
             return s[n-1]
+        
+        
+        
+let num = 0;    // 用来记录fib函数执行次数，执行一次加一
+function fib(n) {
+  num ++;
+  if(n === 0) {
+    return 0;
+  }
+  if(n === 1) {
+    return 1;
+  }
+  return fib(n-1) + fib(n-2);
+}
