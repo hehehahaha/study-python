47804bee9e4b9f650091082894bd09d94b41c189
hehehahaha <gejiexian@126.com>
2019-09-10 17:09:23 +0800

    Create BFS.py

---
+++ BFS.py
@@ -0,0 +1,24 @@
+#建立图
+graph = {
+   "A": ["B", "C"],
+   "B": ["A", "C", "D"],
+   "C": ["A", "B", "D", "E"],
+   "D": ["B", "C", "E", "F"],
+   "E": ["C", "D"],
+   "F": ["D"]
+}
+
+def BFS(graph, s):
+   queue = []
+   queue.append(s)
+   seen = set()
+   seen.add(s)
+   while (len(queue) > 0):
+      vertex = queue.pop(0)
+      nodes = graph[vertex]   #取邻接点
+      for w in nodes:
+         if w not in seen:
+            queue.append(w)
+            seen.add(w)
+      print(vertex)
+BFS(graph, "F")
