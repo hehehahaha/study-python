ca9378050a071b8f33452dcf0f1abda0dc4a9c25
hehehahaha <gejiexian@126.com>
2019-09-12 10:05:08 +0800

    Update 01bag.py

--- 01bag.py
+++ 01bag.py
@@ -1,9 +1,8 @@
 #01
-import numpy as np
 N = 6
 W = 21
 def knapsack(N, W):
-   B = np.ones((N,W))*0
+   B = [[0 for i in range(W)] for j in range(N)]
    w = [0, 2, 5, 4, 3, 9]
    v = [0, 3, 8, 5, 4, 10]
    for i in range(1,N):
@@ -11,12 +10,7 @@ def knapsack(N, W):
          if w[i] > j:
             B[i][j] = B[i-1][j]
          else:
-            value1 = B[i-1][j-w[i]] + v[i]
-            value2 = B[i-1][j]
-            if value1 > value2:
-               B[i][j] = value1
-            else:
-               B[i][j] = value2
+            B[i][j] = max(B[i-1][j-w[i]] + v[i], B[i-1][j])
    return B[5][20]
 a = knapsack(N, W)
 print(a)
