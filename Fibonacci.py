cbd5a1ae11fca4e611c925d41c4ee148c3f75756
hehehahaha <gejiexian@126.com>
2019-09-15 16:24:55 +0800

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -2,21 +2,16 @@ def Fibonacci(n):
   s = [] * n
   s.append(1)
   s.append(1)
-  for i in range(2, n):
+  for i in range(2, n+1):
     s.append(s[i-1]+s[i-2])
-  return s[n-1]
+  return s[n]
+
+#递归
+def Fibonacci(n):
+  if n == 0:
+    return 1
+  if n == 1:
+    return 1
+  return Fibonacci(n-1) + Fibonacci(n-2)
+  
 print(Fibonacci(10))
-        
-        
-#递归        
-let num = 0;    // 用来记录fib函数执行次数，执行一次加一
-function fib(n) {
-  num ++;
-  if(n === 0) {
-    return 0;
-  }
-  if(n === 1) {
-    return 1;
-  }
-  return fib(n-1) + fib(n-2);
-}
